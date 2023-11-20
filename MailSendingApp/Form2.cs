1aeed5c28461b17962cf13d68d7e0ef8c3ea48cc
Vihanga <vkalansooriya1997@gmail.com>
2023-11-20 16:24:40 +0530

    Update .gitignore

--- MailSendingApp/Form2.cs
+++ MailSendingApp/Form2.cs
@@ -11,24 +11,26 @@ using System.Windows.Forms;
 
 namespace MailSendingApp
 {
+    
     public partial class Form2 : Form
     {
-        string connectionString = "Data Source=DESKTOP-AUL1S01;Initial Catalog=Test01;User ID=pll;Password=myposadminauthentication";
+        private mailApp mailAppInstance;
         public Form2()
         {
             InitializeComponent();
+            InitializeButtonDesign();
+            mailAppInstance = new mailApp();
             DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn
             {
-                HeaderText = "Select", 
+                HeaderText = "SELECT", 
                 Name = "Select",
             };
-
             dataGridView1.Columns.Insert(0, checkBoxColumn); 
             dataGridView1.AllowUserToAddRows = false;
 
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(Globalconfig.ConnectionString))
                 {
                     connection.Open();
 
@@ -40,17 +42,41 @@ namespace MailSendingApp
                         {
                             DataTable dataTable = new DataTable();
                             adapter.Fill(dataTable);
-
                             dataGridView1.DataSource = dataTable;
                         }
                     }
-
                 }
 
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    if (column.Name != "Select")
+                    {
+                        column.ReadOnly = true;
+                    }
+                }
+                DataGridViewColumn selectColumn = dataGridView1.Columns["Select"];
+                if (selectColumn != null)
+                {
+                    selectColumn.DisplayIndex = 5;
+                }
+
+                DataGridViewCellStyle headerStyle = new DataGridViewCellStyle(dataGridView1.ColumnHeadersDefaultCellStyle);
+                headerStyle.Font = new Font("Arial", 8, FontStyle.Bold); 
+                dataGridView1.ColumnHeadersDefaultCellStyle = headerStyle;
+
+                dataGridView1.Columns["TB_ID"].HeaderText = "ID";
+                dataGridView1.Columns["TB_RECEIVERMAIL"].HeaderText = "RECEIVER EMAIL";
+                dataGridView1.Columns["TB_LOCATION"].HeaderText = "LOCATION";
+                dataGridView1.Columns["TB_TYPE"].HeaderText = "TYPE";
+                dataGridView1.Columns["TB_RUNNO"].HeaderText = "REPORT NUMBER";
+
+                dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                Logger.LogError("Error in Form2 :", ex);
             }
         }
 
@@ -61,7 +87,7 @@ namespace MailSendingApp
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'test01DataSet2.TB_MAILDETAILS' table. You can move, or remove it, as needed.
+            
             this.tB_MAILDETAILSTableAdapter.Fill(this.test01DataSet2.TB_MAILDETAILS);
 
         }
@@ -71,32 +97,33 @@ namespace MailSendingApp
             Application.Exit();
         }
 
-        private void btn_send_Click(object sender, EventArgs e)
+        private async void btn_send_Click(object sender, EventArgs e)
         {
-            foreach (DataGridViewRow row in dataGridView1.Rows)
+            try
             {
-                DataGridViewCheckBoxCell checkBoxCell = row.Cells["Select"] as DataGridViewCheckBoxCell;
-                if (checkBoxCell != null && (bool)checkBoxCell.EditedFormattedValue)
+                btn_send.Enabled = false;
+                btn_deselectall.Enabled = false;
+                btn_selectall.Enabled = false;
+                btn_refresh.Enabled = false;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
                 {
-                    // This row is checked
-                    int tbId = Convert.ToInt32(row.Cells["TB_ID"].Value);
-                    string receiverMail = row.Cells["TB_RECEIVERMAIL"].Value.ToString();
-                    // ... and so on for other columns
-
-                    // Perform your desired actions with the checked data
+                    DataGridViewCheckBoxCell checkBoxCell = row.Cells["Select"] as DataGridViewCheckBoxCell;
+                    if (checkBoxCell != null && (bool)checkBoxCell.EditedFormattedValue)
+                    {
+                        int id = Convert.ToInt32(row.Cells["TB_ID"].Value);
+                        string recipientEmail = row.Cells["TB_RECEIVERMAIL"].Value.ToString();
+                       
+                        await mailAppInstance.ProcessEmailAsync(recipientEmail, id);
+                        btn_send.Enabled = true;
+                        btn_deselectall.Enabled = true;
+                        btn_selectall.Enabled = true;
+                        btn_refresh.Enabled=true;
+                    }
                 }
             }
-        }
-
-        private void btn_selectall_Click(object sender, EventArgs e)
-        {
-            foreach (DataGridViewRow row in dataGridView1.Rows)
+            catch (Exception ex)
             {
-                DataGridViewCheckBoxCell checkBoxCell = row.Cells["Select"] as DataGridViewCheckBoxCell;
-                if (checkBoxCell != null)
-                {
-                    checkBoxCell.Value = true;
-                }
+                Logger.LogError("Unhandled exception in the application", ex);
             }
         }
 
@@ -116,7 +143,7 @@ namespace MailSendingApp
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(Globalconfig.ConnectionString))
                 {
                     connection.Open();
 
@@ -130,6 +157,7 @@ namespace MailSendingApp
                             adapter.Fill(dataTable);
 
                             dataGridView1.DataSource = dataTable;
+                            dateTimePicker1.Value = DateTime.Now.Date;
                         }
                     }
                 }
@@ -137,6 +165,54 @@ namespace MailSendingApp
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                Logger.LogError("Unhandled exception in the application", ex);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = dateTimePicker1.Value.Date; 
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Globalconfig.ConnectionString))
+                {
+                    connection.Open();
+               
+                    string query = "SELECT TB_ID, TB_RECEIVERMAIL, TB_LOCATION, TB_TYPE, TB_RUNNO " +
+                                   "FROM TB_MAILDETAILS " +
+                                   "WHERE TB_STATUS = 0 AND CONVERT(DATE, TB_DATE) = @SelectedDate";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@SelectedDate", selectedDate);
+
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Logger.LogError("Unhandled exception in the application", ex);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DataGridViewCheckBoxCell checkBoxCell = row.Cells["Select"] as DataGridViewCheckBoxCell;
+                if (checkBoxCell != null)
+                {
+                    checkBoxCell.Value = true;
+                }
             }
         }
     }
--- MailSendingApp/Properties/Resources.Designer.cs
+++ MailSendingApp/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MailSendingApp.Properties
-{
-
-
+namespace MailSendingApp.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,84 @@ namespace MailSendingApp.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MailSendingApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _334261_200 {
+            get {
+                object obj = ResourceManager.GetObject("_334261_200", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _4604262_200 {
+            get {
+                object obj = ResourceManager.GetObject("_4604262_200", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _773581_200 {
+            get {
+                object obj = ResourceManager.GetObject("_773581_200", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _984576_200 {
+            get {
+                object obj = ResourceManager.GetObject("_984576_200", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
